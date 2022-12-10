912ff2b6848497cd0f3be36f33300028415e87df
Harsh Narayan <narayanharsh2000@gmail.com>
2022-12-10 15:52:25 +0530

    task 4

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Address from './Components/Address';
 import Banner from './Components/Banner';
+import Connector from './Components/Connector';
 import Exhibit from './Components/Exhibit';
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
       </Exhibit>
       <Exhibit name="Public IPv6 Address">
         <Address url='https://api64.ipify.org?format=json' />
+      </Exhibit>
+      <Exhibit name="Pylon Packet Latency">
+        <Connector />
       </Exhibit>
     </div>
   );

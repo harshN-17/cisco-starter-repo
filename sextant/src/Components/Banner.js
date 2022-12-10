ed1ab26970cb72cf7d06ba61b425060d6ce32afc
Harsh Narayan <narayanharsh2000@gmail.com>
2022-12-10 13:16:52 +0530

    task 2

---
+++ sextant/src/Components/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Banner.css';
+
+const Banner = () => {
+  return (
+    <div className='banner__wrapper'>
+      <div className='banner__title'>Sextant</div>
+    </div>
+  )
+}
+
+export default Banner
---
+++ sextant/src/Components/Exhibit.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Exhibit = () => {
+  return (
+    <div>
+      <h1>Heading</h1>
+      <h3>This is children</h3>
+    </div>
+  )
+}
+
+export default Exhibit
